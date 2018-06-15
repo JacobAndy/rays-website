1e0a80e3d6f0a92ff0810ea1d2a3bd35c31e112f
Jacob Anderson <odistiinct@gmail.com>
2018-06-15 17:27:09 -0500

    footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <main className="footer-main">
+      <h4>
+        © Ray McCollum Celebration Church of Nashville Designed by Jacob
+        Anderson
+      </h4>
+    </main>
+  );
+}
+export default Footer;
