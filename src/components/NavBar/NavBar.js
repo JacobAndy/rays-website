2d273540982976c3a8c89f3ea99f99f08cf9b8b0
Jacob Anderson <odistiinct@gmail.com>
2018-06-16 08:16:18 -0500

    navbar mobile responsive set up

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,29 +3,42 @@ import "./NavBar.css";
 import { Link } from "react-router-dom";
 
 class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      burgerToggle: false
+    };
+  }
   render() {
+    let { burgerToggle } = this.state;
     return (
       <div className="navbar-holder">
-        <section className="navbar-info">
+        <section
+          className={
+            burgerToggle
+              ? "navbar-info navbar-info-slide-mobile"
+              : "navbar-info"
+          }
+        >
           <Link to="/courses">
             <p>Courses</p>
           </Link>
-          <Link to="">
+          <Link to="/blogs">
             <p>Blogs</p>
           </Link>
-          <Link to="">
+          <Link to="/store">
             <p>Store</p>
           </Link>
           <Link to="/">
             <h4>Ray McCollum</h4>
           </Link>
-          <Link to="">
+          <Link to="/messages">
             <p>Messages</p>
           </Link>
-          <Link to="">
+          <Link to="/donate">
             <p>Donate</p>
           </Link>
-          <Link to="">
+          <Link to="/contact">
             <p>Contact</p>
           </Link>
         </section>
