05acd61c7ec0a15f2c7aad290df98d23b033873b
josephiznot <josephiznot@gmail.com>
2018-07-14 09:00:09 -0500

    trying to resolve merge conflict

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NavBar from "./components/NavBar/NavBar";
 
 class App extends Component {
   render() {
+    console.log("i hope this works");
     return (
       <Router>
         <Fragment>
